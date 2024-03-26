8a4b692f72f5602742b50870958b20b83871c0bf
couvong <couvong3@yandex.ru>
2024-03-26 17:24:12 +0300

    +Geometry module

--- OPILab/OPILab.cpp
+++ OPILab/OPILab.cpp
@@ -23,3 +23,22 @@ bool ValidatePoints(const vector<Point>& points)
 {
     return true;
 }
+
+pair<double, double> FindLine(const Point& p1, const Point& p2) 
+{
+    double m = (p2.y - p1.y) / (p2.x - p1.x);
+    double c = p1.y - m * p1.x;
+
+    return { m, c };
+}
+
+int PointOfLine(const Point& point, const pair<double, double>& line) 
+{
+    double result = line.first * point.x - point.y + line.second;
+    if (result > 0)
+        return 1;
+    else if (result < 0)
+        return -1;
+    else
+        return 0;
+}
